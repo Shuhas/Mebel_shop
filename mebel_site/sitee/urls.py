b194d8ce2a0a2747d48229812379d2b119bfb701
Shuhas <as.shuhas@gmail.com>
2022-02-16 20:23:21 +0500

    html fayllar ulandi

--- mebel_site/sitee/urls.py
+++ mebel_site/sitee/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('contact/', contacts, name='contacts'),
+    path('catalog/', catalog, name='catalog'),
+    path('product/', product, name='product'),
 ]
