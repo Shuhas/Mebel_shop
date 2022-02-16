ada303b1017005ba3799a74b14ed9d03a3d061c7
Shuhas <as.shuhas@gmail.com>
2022-02-16 17:27:53 +0500

    Databaza yaratib, ulab oldim.

--- mebel_site/src/settings.py
+++ mebel_site/src/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mebel_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mebel_db',
+        'USER': 'postgres',
+        'PASSWORD': '9424',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
