198ad8ae63b85d0fac4f754ff17fe55415ffe4b5
Shuhas <as.shuhas@gmail.com>
2022-02-17 15:12:00 +0500

    Category va Product modellarini yaratib oldim.

---
+++ mebel_site/sitee/__init__.py
@@ -0,0 +1,14 @@
+def price_choice():
+    return [
+        ('$', 'USD'),
+        ("So'm", 'UZS'),
+        ('₽', 'RUB'),
+    ]
+
+
+def color_choice():
+    return [
+        ('qizil', 'red'),
+        ("ko'k", 'blue'),
+        ('sariq', 'yellow'),
+    ]
--- mebel_site/sitee/models.py
+++ mebel_site/sitee/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from mebel_site.sitee import price_choice, color_choice
+
+
+class Category(models.Model):
+    content = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=256)
+    icon = models.ImageField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.content)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.content
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.IntegerField()
+    price_type = models.CharField(max_length=5, choices=price_choice())
+    color = models.CharField(max_length=64, choices=color_choice())
+    size = models.JSONField(default={'len': 0, 'high': 0, 'eni': 0})
+    short_description = models.TextField()
+    ctg = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
